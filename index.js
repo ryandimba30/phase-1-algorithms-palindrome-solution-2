429f4f098711e3f53bea073ad95427150d4826f7
ryandimba <ryan.dimba@student.moringaschool.com>
2022-08-29 10:25:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //iterate through the word to the middle
+  for (let i = 0; i < word.length / 2; i++) {
+    //compare the letters with the corresponding characters
+    let j = word.length - i - 1;
+    // if the letters are different, then return false
+    if (word[i] !== word[j]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  iterate through the word to the middle
+  write a conditional statement
+  return true if all the letters are the same
+  if any of the letters are not the same, return false
 */
 
 /*
   Add written explanation of your solution here
+  i need to write a function that returns true if an input string is same as the output string
+  that means that if the first letter of the input string is same as the last letter of the output string and the second letter
+  is the same as the second last letter of the output string, i should return true
+  I need to iterate over the input string to the middle and check if any of the characters  are different,
+  i return false
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +39,31 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("peter"));
 }
 
 module.exports = isPalindrome;
